ad8cc7b5120bccb294efc9425e6218de283462d6
AnaVitorio <65178482+AnaVitorio@users.noreply.github.com>
2020-06-16 15:11:16 -0300

    Add files via upload
    Adição de comandos para que o usuário possa jogar quantas vezes quiser

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -4,25 +4,34 @@ import random
 def cls():
     os.system('cls')
 
-numero_aleatorio = random.randint(0, 10)
-#print(numero_aleatorio)
-acertou = False
-while not acertou:
-    a = int(input("Chute um número entre 0 e 10:"))
-    cls()
-    if a == numero_aleatorio:
-        acertou = True
-        print("Parabéns!!! Você advinhou o número escolhido -- {}".format(numero_aleatorio))
-    else:
-        if a > numero_aleatorio:
-            print("Não foi dessa vez!!")
-            print("Tente um número menor")
-        elif a < numero_aleatorio:
-            print("Não foi dessa vez!!")
-            print("Tente um número maior")
+def chute_numero():
+    numero_aleatorio = random.randint(0, 10)
+    #print(numero_aleatorio)
+    acertou = False
+    while not acertou:
+        a = int(input("Chute um número entre 0 e 10:"))
+        cls()
+        if a == numero_aleatorio:
+            acertou = True
+            print("Parabéns!!! Você advinhou o número escolhido -- {}".format(numero_aleatorio))
 
-print("Fim")
+        else:
+            if a > numero_aleatorio:
+                print("Não foi dessa vez!!")
+                print("Tente um número menor")
+            elif a < numero_aleatorio:
+                print("Não foi dessa vez!!")
+                print("Tente um número maior")
+    jogar_nova = int(input('Você quer jogar novamente? (1)-sim / (2)-não'))
+    if jogar_nova == 1:
+        cls()
+        chute_numero()
+    elif jogar_nova == 2:
+        cls()
+        print("Fim")
+    
 
+chute_numero()
 
 
 
