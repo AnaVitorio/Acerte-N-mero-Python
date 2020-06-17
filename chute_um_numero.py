4061eccb853521a13cc992ca7e2e6337f919cf32
AnaVitorio <65178482+AnaVitorio@users.noreply.github.com>
2020-06-17 13:30:55 -0300

    Add files via upload
    Adição de uma estrutura for, adição de um aviso inicial e de um aviso de fim de jogo

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -6,30 +6,33 @@ def cls():
 
 def chute_numero():
     numero_aleatorio = random.randint(0, 10)
-    #print(numero_aleatorio)
-    acertou = False
-    while not acertou:
+    print('-------VOCÊ TEM 3 CHANCES DE ACERTAR O NÚMERO SORTEADO------------')
+
+    for tentativa in range(1, 4):
         a = int(input("Chute um número entre 0 e 10:"))
         cls()
-        if a == numero_aleatorio:
-            acertou = True
-            print("Parabéns!!! Você advinhou o número escolhido -- {}".format(numero_aleatorio))
-
+        if a > numero_aleatorio:
+            print("Tente um número menor")
+        elif a < numero_aleatorio:
+            print("Tente um número maior")
         else:
-            if a > numero_aleatorio:
-                print("Não foi dessa vez!!")
-                print("Tente um número menor")
-            elif a < numero_aleatorio:
-                print("Não foi dessa vez!!")
-                print("Tente um número maior")
+            break
+
+    if a == numero_aleatorio:
+        cls()
+        print("Parabéns!!! Você advinhou o número escolhido --> {}".format(numero_aleatorio))
+    else:
+        cls()
+        print('FIM DE JOGO - não foi dessa vez, o número sorteado foi --> {}'.format(numero_aleatorio))
+
     jogar_nova = int(input('Você quer jogar novamente? (1)-sim / (2)-não'))
     if jogar_nova == 1:
         cls()
         chute_numero()
     elif jogar_nova == 2:
         cls()
-        print("Fim")
-    
+        print("Tchau")
+
 
 chute_numero()
 
